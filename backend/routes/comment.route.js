c26e5e2734bbbe6069f36e815a082d543285a9fe
Vidya0907 <vidya09.gowda@gmail.com>
2025-02-23 11:17:53 +0530

    123

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,10 +1,14 @@
-import express from "express"
-import { addComment, deleteComment, getPostComments } from "../controllers/comment.controller.js"
+import express from "express";
+import {
+  addComment,
+  deleteComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/:postId", getPostComments)
-router.post("/:postId", addComment)
-router.delete("/:id", deleteComment)
+router.get("/:postId", getPostComments);
+router.post("/:postId", addComment);
+router.delete("/:id", deleteComment);
 
-export default router 
+export default router;
